75f4920d272715302437d2fd9950567501032d10
ChristianPRO1982 <christian.martiny@outlook.com>
2024-12-06 16:37:23 +0100

    .

---
+++ src/get_final_url.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import json
+import time
+
+def get_final_url_with_selenium(url):
+    driver = None
+    try:
+        # Configuration du driver
+        options = Options()
+        options.headless = True  # Exécute sans interface graphique
+        driver_path = "/usr/local/bin/chromedriver"  # Remplacez par le chemin réel
+        
+        service = Service(driver_path)
+        driver = webdriver.Chrome(service=service, options=options)
+        
+        # Ouvrir l'URL
+        driver.get(url)
+        time.sleep(1)  # Attendre les redirections ou scripts dynamiques
+        
+        # Obtenir l'URL finale
+        final_url = driver.current_url
+        return final_url
+    except Exception as e:
+        print(f"Erreur : {e}")
+        return None
+    finally:
+        if driver:
+            driver.quit()
+
+
+# scan json file and get final url
+def scan_json_and_get_final_urls(json_file_path):
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+    
+    ai_tools = []
+
+    for item in data:
+        if 'link' in item:
+            dict_ai_tool = {}
+            dict_ai_tool['category'] = item['category']
+            dict_ai_tool['title'] = item['title']
+            dict_ai_tool['description'] = item['description']
+            dict_ai_tool['tags'] = item['tags']
+            dict_ai_tool['link'] = item['link']
+            dict_ai_tool['type'] = item['type']
+
+            url = item['link']
+            final_url = get_final_url_with_selenium(url)
+            print(f"Original URL: {url} -> Final URL: {final_url}")
+            dict_ai_tool['final_url'] = final_url
+
+            ai_tools.append(dict_ai_tool)
+    
+    with open('final_ai_tools.json', 'w') as outfile:
+        json.dump(ai_tools, outfile, indent=4)
+
+json_file_path = 'ai_tools.json'
+scan_json_and_get_final_urls(json_file_path)
---
+++ veille/spiders/ai_tools.py
@@ -0,0 +1,107 @@
+import scrapy
+import json
+
+
+
+common_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Accept-Language': 'fr',
+}
+
+
+class AiToolsSpider(scrapy.Spider):
+    name = "ai_tools"
+    allowed_domains = ["www.aixploria.com"]
+    
+
+    def start_requests(self):
+        with open('categories.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        # Récupérer les liens depuis le JSON
+        if 1 == 0:
+            entry = data[0]
+            url = entry.get('link')
+            category = entry.get('category')
+            link_text = entry.get('link_text')
+            if url:
+                yield scrapy.Request(
+                    url,
+                    headers=common_headers,
+                    callback=self.parse,
+                    meta={
+                        'category': category,
+                        'link_text': link_text,
+                        }
+                    )
+        else:
+            for entry in data:
+                url = entry.get('link')
+                category = entry.get('category')
+                link_text = entry.get('link_text')
+                if url:
+                    yield scrapy.Request(
+                        url,
+                        headers=common_headers,
+                        callback=self.parse,
+                        meta={
+                            'category': category,
+                            'link_text': link_text,
+                            }
+                        )
+
+
+    def parse(self, response):
+        category = response.meta.get('category') + " [" + response.meta.get('link_text') + "]"
+        self.logger.info(f"Scraping category page: {response.url} (Category: {category})")
+
+        div_latest_posts = response.css('div.latest-posts:not(.lateday)')
+
+        div_tools = div_latest_posts.css('div.post-item')
+        for tool in div_tools:
+            # TYPE : free, freemium, paid
+            div_post_thumbnail = tool.css('div.post-thumbnail')
+            divs = div_post_thumbnail.css('div')
+            div = divs[1]
+            span = div.css('span')
+            span = span[1]
+            span = span.css('span')
+            span = span[0]
+            type = span.css('span::text').get()
+
+            # TITLE
+            title = tool.css('a#specialButton::text').get()
+            
+            # DESCRIPTION
+            description = tool.css('p.post-excerpt::text').get()
+
+            # TAGS
+            tags_bloc = tool.css('span.post-category')
+            tags = tags_bloc.css('a')
+            tags_list = []
+            for tag in tags:
+                tags_list.append(tag.css('a::text').get())
+            
+            # LINK
+            link = tool.css('a.visit-site-button4::attr(href)').get()
+
+            yield {
+                'category': category,
+                'title': title,
+                'description': description,
+                'tags': tags_list,
+                'link': link,
+                'type': type,
+            }
+
+        # NEXT PAGE
+        next_page = response.css('a.next.page-numbers::attr(href)').get()
+        if next_page:
+            self.logger.info(f"Next page found: {next_page}")
+            yield scrapy.Request(
+                url=next_page,
+                headers=common_headers,
+                callback=self.parse,
+                meta=response.meta
+            )
+    
